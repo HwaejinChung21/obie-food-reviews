357c98c83a529f37e60a21440856a9c383dd13b6
Hwaejin Chung <hchung@oberlin.edu>
2026-01-12 21:50:23 -0500

    implemented sectionlist to display each menu per station

--- mobile/obie-food-reviews/app/(app)/feed/index.tsx
+++ mobile/obie-food-reviews/app/(app)/feed/index.tsx
@@ -1,6 +1,7 @@
-import { View, Text, FlatList, ActivityIndicator } from 'react-native'
+import { View, Text, FlatList, ActivityIndicator, SectionList } from 'react-native'
 import { useState, useEffect } from 'react'
 import React from 'react'
+import { API_BASE_URL } from '../../../config/api';
 
 const Index = () => {
 
@@ -14,13 +15,13 @@ const Index = () => {
                 setLoading(true);
                 setError(null);
 
-                const response = await fetch('http://192.168.2.140:3000/menus');
+                const response = await fetch(`${API_BASE_URL}?hall=Stevenson&meal=dinner&date=2026-01-12`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
 
                 const data = await response.json();
-                setMenuData(data);
+                setMenuData(Array.isArray(data.stations) ? data.stations : []);
             } catch (err: any) {
                 setError(err?.message ?? 'Unknown error');
             } finally {
@@ -46,22 +47,29 @@ const Index = () => {
         );
     }
 
+    const sections = menuData.map((s) => ({
+        title: s.name,
+        data: s.items,
+    }));
+
      
     return (
         <View>
             <Text>Feed Screen</Text>
-            {menuData && (
-                <FlatList
-                data={menuData}
-                keyExtractor={(item) => item.id.toString()}
+            <SectionList
+                sections={sections}
+                contentContainerStyle={{ padding: 16 }}
+                keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                    <View className="p-8">
-                        <Text className="font-medium">{item.name}</Text>
-                        <Text className="font-medium">{item.stationName}</Text>
+                    <View>
+                        <Text className="text-sm m-2 pl-4">{item.name}</Text>
                     </View>
                 )}
+                renderSectionHeader={({ section }) => (
+                    <Text className="font-bold text-xl m-2">{section.title}</Text>
+                )}
+            
             />
-            )}
         </View>
   )
 }
