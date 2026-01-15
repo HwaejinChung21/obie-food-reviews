32da1e36e1c00b0f98f5359243b66510849a6994
Hwaejin Chung <hchung@oberlin.edu>
2026-01-15 16:51:29 -0500

    testing profiles controller

--- backend/obie-food-reviews/src/app.module.ts
+++ backend/obie-food-reviews/src/app.module.ts
@@ -5,11 +5,12 @@ import { HealthController } from './health.controller';
 import { MenusModule } from './menus/menus.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth/auth.controller';
+import { ProfilesController } from './profiles/profiles.controller';
 
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true}), MenusModule],
-  controllers: [AppController, HealthController, AuthController],
+  controllers: [AppController, HealthController, AuthController, ProfilesController],
   providers: [AppService],
 })
 export class AppModule {}
