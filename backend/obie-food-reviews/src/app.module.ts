cb7e66c49e0039b91039684452104581aff8ffcc
Hwaejin Chung <hchung@oberlin.edu>
2026-01-16 15:53:32 -0500

    testing ratings controller

--- backend/obie-food-reviews/src/app.module.ts
+++ backend/obie-food-reviews/src/app.module.ts
@@ -6,11 +6,12 @@ import { MenusModule } from './menus/menus.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth/auth.controller';
 import { ProfilesModule } from './profiles/profiles.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true}), MenusModule, ProfilesModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true}), MenusModule, ProfilesModule, RatingsModule],
   controllers: [AppController, HealthController, AuthController],
   providers: [AppService],
 })
