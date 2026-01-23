cb30bbfd345980b743152f87c6d0e3ca54f70de1
Hwaejin Chung <hchung@oberlin.edu>
2026-01-23 18:39:34 -0500

    enabled CORS for web

--- backend/obie-food-reviews/src/main.ts
+++ backend/obie-food-reviews/src/main.ts
@@ -5,6 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:19006',
+      'https://localhost:8081',
+      'https://obie-food-reviews--yv1ivr399t.expo.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port, '0.0.0.0');
 
